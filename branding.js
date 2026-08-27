e1c57654a46981951b59a4dc4a34e784859395b7
Alexandre <59087186+Alex-2020-pr@users.noreply.github.com>
2026-08-27 08:32:04 -0300

    Add branding configuration for AM2 PACS

---
+++ branding.js
@@ -0,0 +1,51 @@
+window.PACS_BRANDING = {
+
+    systemName:
+        "AM2 PACS",
+
+    institutionName:
+        "AM2 Soluções",
+
+    logo:
+        "/logoam2.png",
+
+    loginBackground:
+        "/login-background.jpg",
+
+    primaryColor:
+        "#0877C9",
+
+    primaryDark:
+        "#063A67",
+
+    titleLine1:
+        "Tecnologia que",
+
+    titleLine2:
+        "conecta imagens,",
+
+    titleHighlight:
+        "diagnósticos e vidas.",
+
+    description:
+        "Plataforma completa para gestão, visualização e compartilhamento de imagens médicas com segurança e eficiência.",
+
+    welcomeTitle:
+        "Bem-vindo(a)!",
+
+    welcomeText:
+        "Entre com suas credenciais para acessar o sistema.",
+
+    showForgotPassword:
+        true,
+
+    forgotPasswordUrl:
+        "/esqueci-senha",
+
+    showPoweredBy:
+        true,
+
+    company:
+        "AM2 Soluções"
+
+};
